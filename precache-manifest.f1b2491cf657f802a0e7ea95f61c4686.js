65c305153bd728448b872d0f9484ad090574721a
c-v_polyatykyn <Vladyslav.Polyatykyn@mindgeekconsultants.com>
2019-09-25 16:45:48 +0300

    Updates

---
+++ precache-manifest.f1b2491cf657f802a0e7ea95f61c4686.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0448d3acb6ae31ac644eebc4ff246a7",
+    "url": "/monsters-pj/index.html"
+  },
+  {
+    "revision": "81a09ee5fa722e4680db",
+    "url": "/monsters-pj/static/css/main.6e5ab675.chunk.css"
+  },
+  {
+    "revision": "19bd55e22f203865015d",
+    "url": "/monsters-pj/static/js/2.e7f6d38f.chunk.js"
+  },
+  {
+    "revision": "81a09ee5fa722e4680db",
+    "url": "/monsters-pj/static/js/main.fea00471.chunk.js"
+  },
+  {
+    "revision": "078cf8c1b55ee3db791e",
+    "url": "/monsters-pj/static/js/runtime-main.9c6aecc6.js"
+  }
+]);
